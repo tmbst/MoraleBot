bd78192cb1ba81400a07921a2c07efbb8d1fbf10
unknown <cayfarrier@gmail.com>
2021-03-06 01:01:25 -0700

    Implemented command handler, ping command

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const Discord = require('discord.js');
+const fs = require('fs');
+const { prefix, token } = require('./config.json');
+
+
 const client = new Discord.Client();
-const config = require('./config.json');
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
-    console.log('Ready!');
+    console.log('Bot is online.');
 });
 
 client.on('message', message => {
-    if (message.content === `${config.prefix}ping`) {
-        message.channel.send('Pong.');
     
-    } else if (message.content === `${config.prefix}beep`) {
-        message.channel.send('Boop.');
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (!client.commands.has(command)) return;
+
+    try {
+        client.commands.get(command).execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('Sorry, there was an error executing that command!');
     }
 });
 
-client.login(config.token);
+client.login(token);
