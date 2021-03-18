e630e13c0ee02170afde52fc76f48ea0818c7b4d
unknown <cayfarrier@gmail.com>
2021-03-17 22:54:03 -0600

    Improved onMemberAdd Event to show years, months, days ago from account creation

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,6 @@
 const database = require('../database/dbFunctions');
 const Discord = require('discord.js');
+const { DateTime } = require('luxon');
 
 module.exports = {
     name: 'guildMemberAdd',
@@ -8,13 +9,15 @@ module.exports = {
 
         // Swap this channelID for the TMBST channelID
         const channelID = '571250035086589973';
-        const userJoinedDate = member.user.createdAt
-
+        const userJoinedDate = DateTime.fromJSDate(member.user.createdAt)
+        const userAccountAge = DateTime.now().diff(userJoinedDate,['years','months','days']).toObject();
+        console.log(userJoinedDate);
+        console.log(userAccountAge);
         const welcomeEmbed = new Discord.MessageEmbed()
                 .setColor('00FF00')
                 .setAuthor(member.user.username + '#' + member.user.discriminator, member.user.displayAvatarURL())
                 .setTitle('Member Joined')
-                .setDescription(`Account Created: ${userJoinedDate}`)
+                .addField('Account Age',`${userAccountAge.years} years, ${userAccountAge.months} months, ${Math.floor(userAccountAge.days)} days`)
                 .setTimestamp();
 
         const channel = member.guild.channels.cache.get(channelID);
