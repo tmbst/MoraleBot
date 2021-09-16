750d09072974e8b1bf5e240e51fb398d9d7fe3a0
Michael Farrier <cayfarrier@gmail.com>
2021-09-16 15:43:22 -0700

    V13 Conversion: ping command. Command no longer used for testing

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,40 +1,19 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+/*
+	Slash Command: Ping
+	Uses Database?: No
+	Description: Pong!
+*/
 
 module.exports = {
-	cooldown : 3,
-	
-    data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Replies with Pong!')
-        .addMentionableOption(option => option.setName('mentionable').setDescription('Mention something')),
+	cooldown: 3,
 
-    async execute(interaction) {
-        const mentionable = interaction.options.getMentionable('mentionable');
+	data: new SlashCommandBuilder()
+		.setName("ping")
+		.setDescription("Replies with Pong!"),
 
-		const button = new MessageButton()
-			.setCustomId('primary')
-			.setLabel('Primary')
-			.setStyle('PRIMARY')
-		
-		const row = new MessageActionRow().addComponents(button);
-
-		await interaction.reply({ content: 'Pong!', components: [row] });
-
-		const collector = interaction.channel.createMessageComponentCollector({ componentType: 'BUTTON', time: 5000 });
-
-		collector.on('collect', i => {
-			if (i.user.id === interaction.user.id) {
-				i.reply(`${i.user.id} clicked on the ${i.customId} button.`);
-			} else {
-				i.reply({ content: `These buttons aren't for you!`, ephemeral: true });
-			}
-		});
-
-		collector.on('end', collected => {
-			row.components[0].setDisabled(true)
-			interaction.editReply({ content: 'Pong!', components: [row] });
-			console.log(`Collected ${collected.size} interactions.`);
-		});
-    },
+	async execute(interaction) {
+		return await interaction.reply({ content: "Pong!" });
+	},
 };
